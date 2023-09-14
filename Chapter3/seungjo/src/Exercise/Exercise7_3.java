a104816464dca4e31e5fce5490c3f3654d20874a
f1v3-dev <f1v3@kakao.com>
2023-09-14 15:01:54 +0900

    Feat: Chapter2 Exercise

---
+++ Chapter3/seungjo/src/Exercise/Exercise7_3.java
@@ -0,0 +1,33 @@
+package Exercise;
+
+public class Exercise7_3 {
+
+    public static void main(String[] args) {
+
+        boolean[] check = new boolean[365];
+
+        int count = 0;
+        while(true) {
+            int birthday = (int) (Math.random() * 365);
+            check[birthday] = true;
+
+            boolean end = false;
+            for (int i = 0; i < check.length; i++) {
+                if (check[i] == true) {
+                    end = true;
+                } else {
+                    end = false;
+                    break;
+                }
+            }
+            count++;
+
+            if (end) {
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
