d60cc14ad34071a9c8e647b91185fc1c4bf95ad2
Huni0819 <orutaku15@gmail.com>
2023-09-14 14:39:09 +0900

    Chapter3 exercise

--- Chapter3/jaehun/exercise/Exercise2.java
+++ Chapter3/jaehun/exercise/Exercise2.java
@@ -2,18 +2,26 @@ package Chapter3.jaehun.exercise;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        int count, previousCount;
+        int count;
+        int previousCount;
         int max;
 
-        count = 0;
         previousCount = 0;
         max = 1;
         for(int i = 1; i <= 10000; i++) {
-            for(int j = 1; j < 1; j++) {
+            count = 0;
+            for(int j = 1; j < i; j++) {
                 if(i % j == 0) {
                     count++;
                 }
             }
+
+            if(count >= previousCount) {
+                max = i;
+                previousCount = count;
+            }
         }
+
+        System.out.println(max);
     }
 }
