2a259e06dd749cab6361846bddbb7ecff27373b0
f1v3-dev <f1v3@kakao.com>
2023-09-15 14:25:32 +0900

    Feat: Chapter 4 Exercise

---
+++ Chapter4/seungjo/src/Exercise/ArrayProcessor.java
@@ -0,0 +1,5 @@
+package Exercise;
+
+public interface ArrayProcessor {
+    double apply( double[] array );
+}
---
+++ Chapter4/seungjo/src/Exercise/Exercise1.java
@@ -0,0 +1,30 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        printCapitalized(input);
+    }
+
+    public static void printCapitalized(String input) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(input.charAt(0));
+        for (int i = 0; i < input.length() - 1; i++) {
+            char ch = input.charAt(i);
+            if (ch == ' ') {
+                sb.append(Character.toUpperCase(input.charAt(i + 1)));
+            } else {
+                sb.append(input.charAt(i + 1));
+            }
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
