9d8fbb3c99310537a7055be8dae91ae96bf518f0
= <ydh4745@gmail.com>
2023-09-15 13:25:11 +0900

    Feat: Exercise, 연습문제 제출
    Exercise, 연습문제 제출
    - Exercise폴더 밑에 연습문제 1, 2, 3, 4, 5, 8번 제출. 6, 7번은 JavaFx라서 안풀음

---
+++ Chapter4/damho/src/Exercise/Exercise5.java
@@ -0,0 +1,11 @@
+package Chapter4.damho.src.Exercise;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface ArrayProcessor {
+    double apply(double[] array);
+}
